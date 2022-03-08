b912644a0e596d7b826e639d4f71a2f0e9923fa8
Adarsh Agrawal <adnitrkl@gmail.com>
2022-03-08 15:26:47 +0530

    prepare the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
@@ -31,8 +33,14 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(notFound);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
